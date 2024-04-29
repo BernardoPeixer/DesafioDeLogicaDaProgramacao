594553c5034e081cef48db553ca39362c6c80e9a
BernardoPeixer <bernardo.peixer.stahelin@gmail.com>
2024-04-29 14:04:25 -0300

    ap3_matrizes ap1.dart

---
+++ ap3_matrizes/ap1.dart
@@ -0,0 +1,34 @@
+import 'dart:math';
+
+import '../utils/terminal_utils.dart';
+
+void main() {
+  var linhas = int.tryParse(syncPrompt('Informe o número de linhas desejado:'));
+  var colunas =
+      int.tryParse(syncPrompt('Informe o número de colunas desejadas:'));
+
+  var matriz = criarMatriz(linhas!, colunas!);
+  imprimirMatriz(matriz);
+}
+
+List<List<int>> criarMatriz(int linhas, int colunas) {
+  Random random = new Random();
+  List<List<int>> matriz = [];
+
+  for (int i = 0; i < linhas; i++) {
+    List<int> linha = [];
+    for (int j = 0; j < colunas; j++) {
+      linha.add(random.nextInt(101)); // Números aleatórios de 0 a 100
+    }
+    matriz.add(linha);
+  }
+
+  return matriz;
+}
+
+void imprimirMatriz(List<List<int>> matriz) {
+  for (List<int> linha in matriz) {
+    print(linha
+        .join(' | ')); // Imprime a linha como uma string separada por espaço
+  }
+}
